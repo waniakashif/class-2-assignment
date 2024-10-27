edc186139b32279ba16a67752a058081bf8e3ba1
Waniakashif <165640696+waniakashif@users.noreply.github.com>
2024-10-27 16:14:12 +0500

    Add files via upload

---
+++ contactus.tsx
@@ -0,0 +1,12 @@
+export default function Contact () {
+    return (
+        <div>
+            <h1 className="text-center bg-sky-500"> Contact us page</h1>
+            {" "}
+            <p> Hello companions this is wania kashif and you can contact
+                for any kind of issues or organization I'm giving. I'll 
+                feel  happy to help you through mail:
+            </p>
+        </div>
+    )
+}
